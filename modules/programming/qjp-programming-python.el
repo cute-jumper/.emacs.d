033518fab607445404e6f68caffaced5524e8459
Junpeng Qiu <qjpchmail@gmail.com>
2016-05-08 15:33:28 -0400

    Update python config and replace elpy

--- modules/programming/qjp-programming-python.el
+++ modules/programming/qjp-programming-python.el
@@ -24,22 +24,46 @@
 
 ;;; Code:
 
-;; Python hook
-(add-hook 'python-mode-hook
-          (lambda ()
-            (local-set-key [(return)] 'electrify-return-if-match)
-            (elpy-mode)
-            (elpy-use-ipython)))
+;; Fix flycheck and virtualenv
+;; Modified from:
+;; https://github.com/lunaryorn/.emacs.d/blob/master/lisp/flycheck-virtualenv.el
 
-;; (require 'pymacs)
-;; (autoload 'pymacs-apply "pymacs")
-;; (autoload 'pymacs-call "pymacs")
-;; (autoload 'pymacs-eval "pymacs" nil t)
-;; (autoload 'pymacs-exec "pymacs" nil t)
-;; (autoload 'pymacs-load "pymacs" nil t)
-;; (autoload 'pymacs-autoload "pymacs")
-;; (pymacs-load "ropemacs" "rope-")
-;; (setq ropemacs-enable-autoimport t)
+;; We can safely declare this function, since we'll only call it in Python Mode,
+;; that is, when python.el was already loaded.
+(declare-function python-shell-calculate-exec-path "python")
+
+(defun qjp-flycheck-virtualenv-set-python-executables ()
+  "Set Python executables for the current buffer."
+  (let ((exec-path (python-shell-calculate-exec-path)))
+    (if (setq-local flycheck-python-pylint-executable
+                    (executable-find "pylint"))
+        (setq-local flycheck-checker 'python-pylint)
+      (when (setq-local flycheck-python-flake8-executable
+                        (executable-find "flake8"))
+        (setq-local flycheck-checker 'python-flake8)))))
+
+(defun qjp-pyvenv-activate-after (directory)
+  "Setup flycheck checker every time virtualenv is changed."
+  (qjp-flycheck-virtualenv-set-python-executables))
+
+(advice-add 'pyvenv-activate :after #'qjp-pyvenv-activate-after)
+
+(with-eval-after-load 'company
+  (add-to-list 'company-backends '(company-anaconda :with company-capf)))
+
+(with-eval-after-load 'python
+  (define-key python-mode-map (kbd "C-c e d") #'pydoc)
+  (define-key python-mode-map (kbd "C-c e w") #'pyvenv-workon))
+
+;; Python hook
+(defun qjp-python-mode-hook ()
+  (subword-mode +1)
+  (indent-guide-mode +1)
+  (pyvenv-mode +1)
+  (anaconda-mode +1)
+  (anaconda-eldoc-mode +1))
+
+(add-hook 'python-mode-hook #'qjp-python-mode-hook)
 
 (provide 'qjp-programming-python)
 ;;; qjp-programming-python.el ends here
