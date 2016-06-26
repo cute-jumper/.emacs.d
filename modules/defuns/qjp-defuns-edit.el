071a85572418770f07623a6a616c40590ec34392
Junpeng Qiu <qjpchmail@gmail.com>
2016-06-25 23:24:20 -0400

    comment-dwim respects paredit-mode

--- modules/defuns/qjp-defuns-edit.el
+++ modules/defuns/qjp-defuns-edit.el
@@ -189,10 +189,12 @@ If no region is selected and current line is not blank and we are not at the end
 then comment current line.
 Replaces default behaviour of comment-dwim, when it inserts comment at the end of the line."
   (interactive "*P")
-  (comment-normalize-vars)
-  (if (and (not (region-active-p)) (not (looking-at "[ \t]*$")))
-      (comment-or-uncomment-region (line-beginning-position) (line-end-position))
-    (comment-dwim arg)))
+  (if (bound-and-true-p paredit-mode)
+      (paredit-comment-dwim arg)
+    (comment-normalize-vars)
+    (if (and (not (region-active-p)) (not (looking-at "[ \t]*$")))
+        (comment-or-uncomment-region (line-beginning-position) (line-end-position))
+      (comment-dwim arg))))
 
 ;; ---------------- ;;
 ;; Eval and replace ;;
